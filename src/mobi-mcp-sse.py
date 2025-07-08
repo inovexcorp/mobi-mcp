4a9c6543bd027e1ca05b9803c07b91cae7ba5494
ben.gould <ben.gould@realmone.com>
2025-07-07 20:36:13 -0400

    initial

---
+++ src/mobi-mcp-sse.py
@@ -0,0 +1,69 @@
+from os import getenv
+
+import dotenv
+from mcp.server import FastMCP
+
+from mobi import MobiClient
+
+
+def init_mobi_client() -> MobiClient:
+    base_url = getenv("MOBI_BASE_URL")
+    username = getenv("MOBI_USERNAME")
+    password = getenv("MOBI_PASSWORD")
+    return MobiClient(base_url, username, password)
+
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    mcp: FastMCP = FastMCP('Mobi MCP Server')
+    mobi: MobiClient = init_mobi_client()
+
+
+    @mcp.tool(name="search_mobi_catalog",
+              description="Search the mobi catalog for records whose metadata contain the provided string")
+    def search_mobi_catalog_for_entities(search_for: str, offset: int, limit: int, types: list[str] | None = None,
+                                         keywords: list[str] | None = None):
+        """
+        Search the Mobi catalog for entities matching the provided metadata criteria.
+
+        This method performs a search in the Mobi catalog based on the given string and
+        returns a list of entities that match the provided string in their metadata.
+        Additional filtering is available through entity types and keywords.
+
+        :param search_for: The substring to search for within the entities' metadata.
+        :param offset: The starting point within the total result set to return.
+        :param limit: The maximum number of entities to return.
+        :param types: A list of entity types to filter the search results, or None for no filtering.
+        :param keywords: A list of keywords to filter the search results, or None for no filtering.
+        :return: Any
+        """
+        return mobi.entity_search(search_for, offset=offset, limit=limit, sort="entityName", ascending=True)
+
+
+    @mcp.tool(name="fetch_ontology_data",
+              description="Fetch ontology data for a given ontology record IRI (not the ontology IRI itself, "
+                          "but the IRI of the 'record' containing the ontology data)")
+    def fetch_ontology_data(ontology_iri: str):
+        """
+        Fetches ontology data for a given ontology record IRI.
+
+        This function retrieves the ontology details associated with the provided
+        IRI (Internationalized Resource Identifier) from the ontology data service.
+        If the process is unsuccessful or the IRI is invalid, it may return None. The IRI of the record is what
+        we're looking for here, not necessarily the IRI of the ontology itself:
+
+        ```"record": {
+                "iri": "https://mobi.com/records#6a535eff-2beb-4749-8549-6bf7de956a4e",
+                ...
+            }
+        ```
+
+        :param ontology_iri: The IRI of the record containing the ontology data which needs to be fetched.
+        :type ontology_iri: str
+        :return: A dictionary containing ontology data if successful, or None otherwise.
+        :rtype: Optional[Dict[Any, Any]]
+        """
+        return mobi.get_ontology_data(ontology_iri)
+
+    # Start MCP server
+    mcp.run(transport="sse")
---
+++ src/mobi.py
@@ -0,0 +1,175 @@
+import json
+import urllib.parse
+from os import getenv
+from typing import Optional, Dict, Any
+
+import dotenv
+import requests
+from requests import RequestException
+
+default_catalogs: str = "http://mobi.com/catalog-local"
+rest_context: str = "mobirest"
+
+
+class MobiClient:
+
+    def __init__(self, base_url: str, username: str, password: str, ignore_cert: bool = False):
+        self.base_url: str = base_url
+        self.username: str = username
+        self.password: str = password
+        self.ignore_cert: bool = ignore_cert
+
+    def get_record(self, record_id: str, catalog_id: str = default_catalogs) -> Optional[Dict[Any, Any]]:
+        """
+        Retrieves a record from the catalog by its identifier.
+
+        Constructs a URL based on the provided `record_id` and `catalog_id` to make
+        a GET request to the REST API to fetch the record. The method returns the
+        retrieved record as a dictionary or `None` if no record is found.
+
+        :param record_id: The unique identifier of the record to retrieve.
+        :type record_id: str
+        :param catalog_id: The unique identifier of the catalog to search within.
+                           If not provided, defaults to `default_catalogs`.
+        :type catalog_id: str
+        :return: A dictionary containing the record data if found, otherwise None.
+        :rtype: Optional[Dict[Any, Any]]
+        """
+        url = (f"{self.base_url}/{rest_context}/catalogs/{urllib.parse.quote(catalog_id, safe='')}"
+               f"/records/{urllib.parse.quote(record_id, safe='')}")
+        return self._make_request(url, "GET")
+
+    def get_ontology_data(self, record_id: str) -> Optional[Dict[Any, Any]]:
+        """
+        Fetches ontology data for a specific record.
+
+        This method constructs a URL to request ontology data corresponding to
+        the provided record ID. It sends a GET request to the URL using the
+        internal `_make_request` method and returns the response data if the
+        request is successful.
+
+        :param record_id: ID of the ontology record to fetch.
+        :type record_id: str
+        :return: Ontology data as a dictionary if the request is successful,
+            otherwise None.
+        :rtype: Optional[Dict[Any, Any]]
+        """
+        url = f"{self.base_url}/{rest_context}/ontologies/{urllib.parse.quote(record_id, safe='')}"
+        return self._make_request(url, "GET")
+
+    def entity_search(self, query: str, catalog_id: str = default_catalogs,
+                      offset: int = 0, limit: int = 100, sort: str = "entityName",
+                      ascending: bool = True, types: list[str] | None = None,
+                      keywords: list[str] | None = None) -> dict:
+        """
+        Perform a search operation for entities within the specified catalog. This method
+        allows filtering through entities using various criteria such as keywords, types,
+        and sorting options. It sends a GET request to the appropriate URL and constructs
+        the request parameters dynamically based on the provided arguments.
+
+        :param query: Search text or query string for finding entities.
+        :type query: str
+        :param catalog_id: ID of the catalog to perform the search in. Uses `default_catalogs`
+                           if not specified.
+        :type catalog_id: str, optional
+        :param offset: Offset index for paginated search results. Defaults to 0.
+        :type offset: int, optional
+        :param limit: Maximum number of entities to return in the search results. Defaults
+                      to 100.
+        :type limit: int, optional
+        :param sort: Field by which to sort the entities. Defaults to "entityName".
+        :type sort: str, optional
+        :param ascending: Whether to sort in ascending order. Defaults to True.
+        :type ascending: bool, optional
+        :param types: List of entity types to filter the search. If None, includes all types by
+                      default.
+        :type types: list[str], optional
+        :param keywords: List of keywords to further filter the entities in the search. If None,
+                         includes all entities matching the query regardless of keywords.
+        :type keywords: list[str], optional
+        :return: A dictionary containing the search results for entities that match the
+                 search criteria.
+        :rtype: dict
+        """
+        url = f"{self.base_url}/{rest_context}/catalogs/{urllib.parse.quote(catalog_id, safe='')}/entities"
+        params = {
+            "offset": offset,
+            "limit": limit,
+            "searchText": query,
+            "sort": sort,
+            "ascending": ascending,
+        }
+        if types:
+            params["type"] = ",".join(types)
+        if keywords:
+            params["keywords"] = ",".join(keywords)
+        # make the request with the constructed parameters
+        return self._make_request(url, "GET", params)
+
+    def _make_request(self, url: str, method: str, params: dict = None) -> Optional[Dict[Any, Any]]:
+        """
+        Sends an HTTP request and processes the response.
+
+        This method performs an HTTP request using the given URL and HTTP method. It can
+        accept optional parameters to include as part of the request. The request uses
+        basic authentication credentials and may optionally skip certificate verification
+        based on the session configuration. It handles various response scenarios, including
+        HTTP errors, empty responses, and unexpected or invalid response formats. If the
+        response is JSON, it attempts to parse and return it.
+
+        :param url:
+            The target URL for the HTTP request.
+        :param method:
+            The HTTP method to use (e.g., 'GET', 'POST', etc.).
+        :param params:
+            Optional dictionary of query parameters to be included in the request. Defaults to None.
+        :return:
+            A dictionary containing the parsed JSON response, or None if the request failed
+            or the response was not a valid JSON.
+        """
+        try:
+            response = requests.request(method, url,
+                                        params=params,
+                                        auth=(self.username, self.password),
+                                        verify=not self.ignore_cert)
+            # Check if request was successful
+            if not response.ok:
+                print(f"HTTP Error {response.status_code}: {response.reason}")
+                print(f"Response content: {response.text[:500]}...")
+                return None
+
+            # Check if response has content
+            if not response.text.strip():
+                print("Warning: Empty response received")
+                return {}
+
+            # Check if response is JSON
+            content_type = response.headers.get('content-type', '').lower()
+            if 'application/json' not in content_type:
+                print(f"Warning: Response is not JSON. Content-Type: {content_type}")
+                print(f"Response content: {response.text[:500]}...")
+                return None
+            else:
+                # Parse JSON
+                try:
+                    return response.json()
+                except json.JSONDecodeError as e:
+                    print(f"JSON decode error: {e}")
+                    print(f"Response content: {response.text[:500]}...")
+                    return None
+
+        except RequestException as e:
+            print(f"Request failed: {e}")
+            return None
+
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    base_url = getenv("MOBI_BASE_URL")
+    username = getenv("MOBI_USERNAME")
+    password = getenv("MOBI_PASSWORD")
+    ignore_cert = getenv("MOBI_IGNORE_CERT")
+    action: MobiClient = MobiClient(base_url, username, password, ignore_cert=ignore_cert == "true")
+    print(action.entity_search("Threat"))
+    print()
+    print(action.get_ontology_data("https://mobi.com/records#6a535eff-2beb-4749-8549-6bf7de956a4e"))
